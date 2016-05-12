1647314fe5c0cc5703dac8ea44bd8cd03f5f7c35
hongyehenry <mengwang@172-16-13-231.DYNAPOOL.NYU.EDU>
2016-05-11 22:04:11 -0400

    inits

--- javascript/controllers/HomeController.js
+++
@@ -1,5 +0,0 @@
-app.controller('HomeController', ['$scope','activities', function($scope,activities) {
-  activities.success(function(data){
-    $scope.activities = data;
-  });
-}]);
--- javascript/services/activities.js
+++
@@ -1,9 +0,0 @@
-app.factory('activities', ['$http', function($http) { 
-  return $http.get('../../activities.json') 
-            .success(function(data) { 
-              return data; 
-            }) 
-            .error(function(err) { 
-              return err; 
-            }); 
-}]);
